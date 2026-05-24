1fe4034b94a5fe3b0677d788389838ebf1cfffa9
Stas <malinovskyistas@gmail.com>
2026-05-24 21:55:54 +0300

    створив самі поїзди(маршрути)

---
+++ src/data/trains.js
@@ -0,0 +1,38 @@
+export const trainsData = [
+    {
+        id: 1,
+        number: "091К",
+        route: "Київ - Львів",
+        departure: "22:37",
+        arrival: "06:26",
+        duration: "7 год 49 хв",
+        price: 450
+    },
+    {
+        id: 2,
+        number: "705К",
+        route: "Київ - Перемишль",
+        departure: "20:14",
+        arrival: "04:54",
+        duration: "8 год 40 хв",
+        price: 850
+    },
+    {
+        id: 3,
+        number: "038Ш",
+        route: "Одеса - Київ",
+        departure: "21:10",
+        arrival: "10:22",
+        duration: "13 год 12 хв",
+        price: 520
+    },
+    {
+        id: 4,
+        number: "712К",
+        route: "Київ - Краматорськ",
+        departure: "06:56",
+        arrival: "14:03",
+        duration: "7 год 7 хв",
+        price: 600
+    }
+];
